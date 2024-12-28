6449f5c865b1160ca5a3c93086da064852b3076d
vikas <vikaskmr2411@gmail.com>
2024-12-29 00:18:31 +0530

    resume

--- frontend/src/components/Resume.jsx
+++ frontend/src/components/Resume.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
   return (
-    <div>
+    <div className="fullscreen-div h-full flex flex-col lg:flex-row justify-between items-center text-center lg:text-start text-white gap-5 lg:gap-28 px-10 lg:px-56">
       Resume
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
